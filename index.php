1555d98c7ce2062f46411dbdb81c2d67d3e149c1
Jellebraaksma <jelle27@hotmail.com>
2020-01-02 15:27:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- <script src="js/scrollto.js"></script> -->
+
+    </head>
+    <body>
+		<div class="wrapper">
+			<ul class="page-nav">
+				<li> <a href="#content1" class="scroll1"> Projectopdracht </a> </li>
+				<li> <a href="#content2" class="scroll2"> Opdrachtgever </a> </li>
+				<li> <a href="#content3" class="scroll3"> Domein </a> </li>
+				<li> <a href="#content4" class="scroll4"> Technologie </a> </li>		
+                <li> <a href="#content5" class="scroll5"> Maatschappelijk belang </a> </li>
+                <li> <a href="#content6" class="scroll6"> Eigen bijdrage </a> </li>	
+                <li> <a href="#sources" class="scroll7"> Bronnen </a> </li>    
+			</ul>
+
+            <script> 
+
+
+                $(".scroll1").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#content1").offset().top -80
+                    }, 1000);
+                });               
+
+                $(".scroll2").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#content2").offset().top -80
+                    }, 1000);
+                });
+
+                $(".scroll3").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#content3").offset().top -80
+                    }, 1000);
+                });
+
+                $(".scroll4").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#content4").offset().top -100
+                    }, 1000);
+                });
+
+                $(".scroll5").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#content5").offset().top -80
+                    }, 1000);
+                });
+
+                $(".scroll6").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#content6").offset().top -80
+                    }, 1000);
+                });
+
+                $(".scroll7").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#sources").offset().top 
+                    }, 1000);
+                });
+            </script>
+
+            <div class="page-header">
+                <img src="images/header.jpeg" alt="image" height="" width="100%">
+                <h1> Smart cycling </h1>
+    
+                <div class="header-content">
+                    <p> Deze opdracht is uitgevoerd in opdracht van de Hanzehogeschool. Het gaat hier om een werkveldopdracht waarbij een aantal zaken in kaart worden gebracht. 
+                    Het onderwerp van deze opdracht is het project dat studenten tijdens het vierde jaar van de studie communicatie multimedia & design moeten uitvoeren. De navigatie geeft weer welke zaken er behandeld worden.</p>
+                </div>
+            </div>
+
+
+            <div class="page-middle">
+                <div id="content1">
+                <h2> <i>  “Op welke wijze kan smartphone gebruik worden ingezet om het fietsverkeer veiliger te maken?” </i> </h2>
+                    <p> Tegenwoordig maakt smartphone gebruik een groot en belangrijk deel uit van ons leven. In Nederland weet men dat het gebruik van smartphones in het verkeer levensgevaarlijk kan zijn, toch worden de gevaren op grote schaal genegeerd. Het is zelfs zo ernstig dat één op de vijf ongelukken onder jongeren te maken heeft met smartphone gebruik. Van de jongeren in Nederland (12-18 jaar) heeft bijna iedereen een smartphone bundel en 77% hiervan heeft aangegeven deze regelmatig op de fiets te gebruiken (Sebbar, 2017). Er zijn op dit moment meerdere partijen bezig met het vinden van een oplossing, de overheid en een groot deel van de Nederlanders wil echter dat er een verbod op appen op de fiets komt, ingaande vanaf 2019 (SAMR, 2017). Met dit project zal er worden gekeken naar welke smartphone functies een gevaar vormen in het fietsverkeer en of er alternatieve oplossingen zijn of dat een verbod daadwerkelijk de beste oplossing is voor dit probleem. </p>
+                </div>
+
+                <div id="content2">
+                    <div class="client-content">
+                        <p> De opdracht komt vanuit het lectoraat User-Centered Design van de Hanzehogeschool. Het lectoraat probeert nieuwe kennis op te doen over het ontwerp van digitale gereedschappen om zo binnen een bepaalde doelgroep voor een verandering te zorgen in kennis en/of gedrag. Hierbij ligt de focus op adaptieve systemen, dat wil zeggen dat de gereedschappen (tools) zich kunnen aanpassen aan de individuen, nieuwe technologie en de maatschappij. Deze systemen worden zo snel mogelijk toegepast in samenwerking met bedrijven uit de creatieve industrie en andere partners (HanzeHogeschool, 2018). 
+                        Daarnaast zijn er nog een aantal stakeholders/secundaire opdrachtgevers, dit zijn New Nexus Mobile en Regionaalorgaan Verkeersveiligheid Friesland. New Nexus Mobile is een bedrijf dat zich al 15 jaar bezig houdt met ICT, voornamelijk applicaties, hierbij probeert ze het gebruikelijke te combineren met nieuwe technologieën.  Regionaalorgaan Verkeersveiligheid Friesland (ROF) houdt zich bezig met het bevorderen van de verkeersveiligheid in Friesland. Dit doen ze doormiddel van campagnes, activiteiten op het gebied van verkeerseducatie en projecten. Daarnaast is het ROF koploper op het gebied van actief en innovatief verbeteren van de verkeersveiligheid (ROF, 2018). </p>
+                    </div>
+                    <div class="client-image">
+                        <img src="images/opdrachtgever.jpeg" alt="image" width="" height="">
+                    </div>
+                </div>
+
+                <div id="content3">
+                    <p> De thema’s die bij dit project horen zijn multitasking, road safety (verkeersveiligheid) en distraction (afleiding). Het domein waar dit project binnen valt is stad van de toekomst. Wat opvalt is dat als we kijken naar het verkeer in de stad van de toekomst, dan ligt de focus voornamelijk op het autoverkeer. Er wordt ontzettend hard gewerkt aan het maken van zelfrijdende auto’s. Het AD stelt dat een autobezitter gemiddeld ongeveer 37.500 uur in zijn leven zal besparen. Daarnaast zal het aantal verkeersongelukken afnemen en er zullen minder auto’s nodig zijn (Kouwenhoven, 2018) 
+                    Als we naar de stad van de toekomst in het algemeen kijken is duurzaamheid ook erg belangrijk. De natuur staat centraal en voertuigen moeten zo min mogelijk belastend hiervoor zijn (Jelyia, 2017) Dit is een extra reden om een slimme oplossing te vinden voor de probleemstelling om fietsgebruik te stimuleren waarbij technologie niet in de weg komt te staan. </p>
+                </div>
+
+                <div id="content4">
+                    <div class="tech-image">
+                      <img src="images/tech.jpeg" alt="image" width="" height="">
+                    </div>
+                    <div class="tech-content"> 
+                        <p> Dit probleem speelt al een langere periode, er zijn daarom ook al een aantal oplossingen bedacht. Eén van die oplossingen is het slimme fietsslot van KPN bedoeld voor kinderen. Het fiets slot staat in verbinding met de smartphone, zodra het fiets slot open is wordt de netwerk verbinding van de smartphone verbroken (Nu.nl, 2017). Een mooi voorbeeld van smartphone integratie om het fiets verkeer veiliger te maken is de Smart Jacket van Vodafone. De smart jacket is puur voor het veilig navigeren door het verkeer in het donker. Zodra de smartphone contact maakt met de Smart Jacket wordt de route geprogrammeerd en wanneer de bestuurder dan naar rechts of links moet, wordt dit weergegeven door middel van rode pijlen op de rug van de fietser. De fietser zelf kan op zijn mouw zien welke kant hij of zij op moet (technology, 2016). Een laatste oplossing voor dit probleem is de smart bicycle, Volata 1C. Dit is een fiets met ingebouwd schermpje en knoppen op het stuur, deze kan door middel van bluetooth verbinden met de smartphone (Ringelestijn, 2017). Deze oplossing is op dit moment nog vrij prijzig maar heeft zeker potentie.  </p>
+                    </div>
+                </div>
+
+                <div id="content5">
+                    <p> Het is urgent en zeker belangrijk voor de maatschappij dat er snel een oplossing komt voor dit probleem. Dit komt met name door het dreigende verbod wat waarschijnlijk voor nieuwe problemen gaat zorgen. Maar ook door de snelle ontwikkelingen binnen de technologie, wie weet wat er straks allemaal mogelijk is met een smartphone. Het verbod zou op den duur de technologie in de weg kunnen gaan staan. Daarom is het belangrijk dat er een goede oplossing komt die een middenweg vormt tussen veiligheid en gemak. </p>
+                </div>
+
+
+                <div id="content6">
+                    <p> Mijn kwaliteiten liggen vanwege mijn huidige studie richting (interactive Media Technologies) voornamelijk in het ontwikkelen van websites. Een groot onderdeel hiervan is het constant bedenken van oplossingen voor problemen waar je tegen aan loopt (trial and error). 
+                    Daarnaast heb ik twee jaar Human Technology gestudeerd (tegenwoordig Industrieel product ontwerp) aan de Hanzehogeschool. Bij deze studie ligt de focus op het doen van onderzoek en een oplossing vinden voor allerlei problemen. Het gaat hierbij zowel om fysieke als digitale producten en zelfs ruimtes. Zo heb ik zelfs een keer een slijterij in Paddenpoel verbeterd. Andere opdrachten die ik met deze studie heb uitgevoerd zijn onder andere voor de ov chipkaart, straatverlichting en een medische applicatie. 
+                    Met mijn huidige studie ben ik vooral bezig geweest met digitale producten voor bijvoorbeeld de bloedbank van Nederland (Sanquin) en Dienst ICT en Uitvoering (DICTU). Producten die ik hierbij gemaakt heb zijn voornamelijk websites, applicaties en dashboards. Door de kennis die ik heb opgedaan bij deze twee studies en het werken met opdrachtgevers denk ik dat ik mooi kan bijdragen aan een oplossing voor dit probleem.  </p>
+
+                    <p> Ik zou van dit project graag willen leren wat de invloed van smartphone gebruik is. Zelf zit ik constant op mijn telefoon, maakt niet uit waar ik me bevind, ook al krijg ik bijvoorbeeld geen berichten dan nog is het een soort automatisme dat ik er even op moet kijken. Ik hoop dat ik mijn eigen smartphone gebruik en daarbij mijn veiligheid kan verbeteren door middel van dit project. </p>
+                </div>
+                <img class="skills" src="images/skills.png" alt="image" width="" height="">
+
+            </div>
+
+            <div class="footer">
+                <ul id="sources">
+                    <h2 class="button"> Bronnen </a> </h2>
+                    <div class="sources-content">
+                        <li>  HanzeHogeschool. (2018, maart 4). Lectoraat User-centered Design. Opgehaald van Hanzehogeschool Groningen University of Applied Sciences : https://www.hanze.nl/nld/onderzoek/kenniscentra/centrum-ondernemerschap/onderzoek/lectoraten/user-centered-design </li>
+                        <li> Jelyia, F. (2017, april 28). Themamaand Future Cities: 5 trends voor de stad van de toekomst. Opgehaald van Duurzaam bedrijfsleven: https://www.duurzaambedrijfsleven.nl/future-cities/22559/themamaand-future-cities-5-trends-voor-de-stad-van-de-toekomst </li>
+                        <li> Kouwenhoven, E. (2018, januari 16). 'We krijgen ruim 4 jaar meer vrije tijd, dankzij de zelfrijdende auto'. Opgehaald van AD: https://www.ad.nl/auto/we-krijgen-ruim-4-jaar-meer-vrije-tijd-dankzij-de-zelfrijdende-auto~a1d97782/ </li>
+                        <li> ROF. (2018, Maart). Regionaal orgaan verkeersveiligheid friesland . Opgehaald van provincie fryslân : https://www.fryslan.frl/beleidsthemas/verkeersveiligheid-rof_3652/item/over-het-rof_1879.html </li>
+                        <li> SAMR. (2017, januari 22). Driekwart van Nederland wil een app-verbod op de fiets. Opgehaald van Rijksoverheid: https://www.rijksoverheid.nl/actueel/nieuws/2017/01/22/driekwart-van-nederland-wil-een-verbod-op-appen-op-de-fiets </li>
+                        <li> Sebbar, E. (2017, januari 24). Smartphone oorzaak van 1 op de 5 fietsongevallen met jongeren. Opgehaald van Veilig verkeer: https://vvn.nl/nieuws/2017/smartphone-oorzaak-van-1-op-de-5-fietsongevallen-met-jongeren </li>
+                        <li> Nu.nl. (2017, juni 21). KPN ontwikkelt slim fietsslot dat smartphone vergrendelt . Opgehaald van Nu.nl: https://www.nu.nl/gadgets/4783179/kpn-ontwikkelt-slim-fietsslot-smartphone-vergrendelt.html </li>
+                        <li> Ringelestijn, T. v. (2017, Maart 11). Eerste indruk Volata 1C: slimme fiets met forse prijs. Opgehaald van Bright.nl: https://www.bright.nl/eerste-indruk/eerste-indruk-volata-1c-slimme-fiets-met-forse-prijs </li>
+                        <li> technology. (2016, maart 9). Smart jacket: veilig navigeren in het verkeer. Opgehaald van Dutch Cowboys: https://www.dutchcowboys.nl/technology/smart-jacket-veilig-navigeren-in-het-verkeer </li> 
+                    </div>
+                </ul>
+
+                <script> 
+                    $(document).ready(function(){
+                        $(".sources-content").hide();
+                        $(".button").show();
+
+                        $('.button').click(function(){
+                        $(".sources-content").slideToggle();
+                        });
+                    });
+                </script>
+
+                <div class="page-footer">
+                    <ul class="footer-content">
+                        <li> Gemaakt door Jelle Braaksma </li>
+                        <li> Studentnummer: 318174 </li>
+                        <li> Klas: CMV4G </li>
+                        <li> Datum: 16-3-2018 </li>
+                    </ul>
+
+                    <ul class="social-media">
+                        <li> <a href="https://www.facebook.com/"> Facebook </a> </li>
+                        <li> <a href="https://www.linkedin.com/in/jelle-braaksma-2774a7108/"> Linkedin </a> </li>
+                    </ul>
+                </div>
+            </div>
+		</div> 
+    </body>
+</html>
